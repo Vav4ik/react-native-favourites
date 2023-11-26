a3aba999fcc524b646a6af66b610aa819d1d208b
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-11-26 10:52:04 +0000

    Navigation setup, nativa stack AllPlaces, AddPlace

--- models/place.js
+++ models/place.js
@@ -1,7 +1,7 @@
 class Place {
-  constructor(title, imageUrl, address, location) {
+  constructor(title, imageUri, address, location) {
     this.title = title;
-    this.imageUrl = imageUrl;
+    this.imageUri = imageUri;
     this.address = address;
     this.location = location; // {lat: 0.115565, lng: 141.1456465}
     this.id = Date.now().toString() + Math.random().toString()
